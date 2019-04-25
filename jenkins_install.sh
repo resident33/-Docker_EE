9e61d3d407175378712a5802129987986c2cf16a
Evhenii Kovtun <fors1991@gmail.com>
2019-04-25 17:41:15 +0300

    add jenkins status

--- jenkins_install.sh
+++ jenkins_install.sh
@@ -12,12 +12,15 @@ apt-get update
 apt-get install openjdk-8-jdk -y
 apt-get install jenkins -y
 
-jenkinspass=$(cat /var/lib/jenkins/secrets/initialAdminPassword)
+jenkinsstatus=$(systemctl status jenkins.service | sed '7,$d')
+echo -e " Jenkins status:\n\e[93m $jenkinsstatus"
 
-echo -e  "Jenkins Activation Password: \e[34m $jenkinpass"
+
+jenkinspass=$(cat /var/lib/jenkins/secrets/initialAdminPassword)
+echo -e  "Jenkins Activation Password: \e[93m $jenkinpass"
 
 ipaddr=$(ifconfig | sed -En 's/127.0.0.1//;s/172.17.*//;s/10.0.*//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
-echo -e "Jenkins server adress: \e[34m $ipaddr:8080" 
+echo -e "Jenkins server adress: \e[93m $ipaddr:8080" 
 
 
 
